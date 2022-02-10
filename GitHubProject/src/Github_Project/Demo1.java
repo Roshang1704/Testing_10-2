71f27bd28288deda76cc814efa957b758a0ef77a
Lenovo <Lenovo@LAPTOP-RM311E4V>
2022-02-10 23:45:31 +0530

    By Rg

--- GitHubProject/src/Github_Project/Demo1.java
+++ GitHubProject/src/Github_Project/Demo1.java
@@ -2,8 +2,19 @@ package Github_Project;
 
 public class Demo1
 {
+	public void m2()
+	{
+		System.out.println("M2");
+	}
+	public void m3()
+	{
+		System.out.println("m3");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hii");
+		Demo1 dd=new Demo1();
+		dd.m2();
+		dd.m3();
 	}
 }
