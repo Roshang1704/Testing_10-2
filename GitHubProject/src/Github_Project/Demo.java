bb2f5be91409f795e8d1c1506894317ea4a64764
Roshang1704 <99422419+Roshang1704@users.noreply.github.com>
2022-02-10 23:33:10 +0530

    By RG

--- GitHubProject/src/Github_Project/Demo.java
+++ GitHubProject/src/Github_Project/Demo.java
@@ -2,8 +2,15 @@ package Github_Project;
 
 public class Demo
 {
+	public void m1()
+	{
+	 	System.out.println("M1");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hiii This is main method");
+		
+		Demo d=new Demo();
+		d.m1();
 	}
 }
