aff6ed8482c83527a4dca96330c8cdd758a8da37
Roshang1704 <99422419+Roshang1704@users.noreply.github.com>
2022-02-10 23:54:34 +0530

    By Sagar

--- GitHubProject/src/Github_Project/Demo.java
+++ GitHubProject/src/Github_Project/Demo.java
@@ -6,11 +6,16 @@ public class Demo
 	{
 	 	System.out.println("M1");
 	}
+	public void m3()
+	{
+	 	System.out.println("M3");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hiii This is main method");
 		
 		Demo d=new Demo();
 		d.m1();
+		d.m3();
 	}
 }
