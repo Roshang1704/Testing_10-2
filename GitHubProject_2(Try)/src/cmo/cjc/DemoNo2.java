a3e9015139b2fe05ae29fad405f8576ea540c226
Lenovo <Lenovo@LAPTOP-RM311E4V>
2022-02-11 17:33:35 +0530

    New

---
+++ GitHubProject_2(Try)/src/cmo/cjc/DemoNo2.java
@@ -0,0 +1,14 @@
+package cmo.cjc;
+
+public class DemoNo2 
+{
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	public static void main(String[] args) 
+	{
+		DemoNo2 d=new DemoNo2();
+		d.m1();
+	}
+}
